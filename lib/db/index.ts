4c8d1f5fc044ef4aa147f172a2d69941d8243ca6
Shariq Ali Asadi <shariq.asadi@gmail.com>
2024-08-10 19:28:42 +0500

    Dockerized db

--- lib/db/index.ts
+++ lib/db/index.ts
@@ -3,7 +3,7 @@ import { NodePgDatabase, drizzle } from 'drizzle-orm/node-postgres';
 import * as schema from './schema';
 
 export const pool = new pg.Pool({
-  connectionString: process.env.DB_URL!,
+  connectionString: process.env.DATABASE_URL!,
 });
 
 export const db = drizzle(pool, { schema }) as NodePgDatabase<typeof schema>;
